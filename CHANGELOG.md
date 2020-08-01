529ffa295fde45389a2614b55dfc090b7923b4cc
José Valim <jose.valim@dashbit.co>
2020-08-01 21:06:03 +0200

    Release v0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## v0.2.0 (2020-08-01)
+
+* Rework all callbacks to receive the pool state
+* Add `handle_enqueue`, `handle_dequeue`, and `handle_update`
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule NimblePool.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @url "https://github.com/dashbitco/nimble_pool"
 
   def project do
